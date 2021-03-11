71a1c1af5045f238e3bbe9e9c1a7c5819b937dd7
Rob-VQD <80262254+Rob-VQD@users.noreply.github.com>
2021-03-11 14:45:21 +0100

    Update vars.tf

--- terraform-code/vars.tf
+++ terraform-code/vars.tf
@@ -14,6 +14,23 @@ variable "prefix" {
     description = "The prefix used to build the elements"
 }
 
-variable "profile" {
-  default = "default"
+variable "IP_address_port_1433" {
+    type = list(string)
+    description = "Give IP addresses to allow traffic to RDS, every IP in string form and total in list form (default is ONLY VQD IP address)"
+}
+
+variable "Database_Password" {
+    type = string
+    description = "Give the Password to login to the database"
+}
+
+variable "Database_Username" {
+    type = string
+    description  = "Give the Username to login to the database"
+}
+
+variable "Port_to_connect_to_db" {
+    type = number
+    description = "Give the port to connect to the db (default 3306)"
+    default = 3306
 }
